21dbc666c21b10a9c4ae629bb882cc069d629721
mukeshyadac <151907863+mukeshyadac@users.noreply.github.com>
2023-11-24 01:44:07 +0530

    Update demo.java
    changed my name by him

--- practice/src/practice/demo.java
+++ practice/src/practice/demo.java
@@ -3,7 +3,13 @@ package practice;
 public class demo {
 
 	public static void main(String[] args) {
-		System.out.println("Demo");
+		System.out.println("ajay");
+               System.out.println("yadav")
 	}
+
+public static void m1(){
+System.out.println("mukesh yadav")
+
+
 
 }
